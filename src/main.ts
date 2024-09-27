5ee7f2e568b98d25dfc46b7b90d79322c6f95acf
David Rodriguez <david.rodriguez03@alumnos.ucn.cl>
2024-09-27 16:41:42 -0300

    login

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:8081',  // Permite el origen del frontend
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(3000);
+  
 }
 bootstrap();
